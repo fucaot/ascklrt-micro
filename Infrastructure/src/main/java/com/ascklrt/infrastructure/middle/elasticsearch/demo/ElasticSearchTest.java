2e3f52b8de695e818cdecb167db2bf6ec0ed7e0a
fucaot <fucaot@qq.com>
2023-11-02 16:04:27 +0800

    elasticsearch client write doc

--- Infrastructure/src/main/java/com/ascklrt/infrastructure/middle/elasticsearch/demo/ElasticSearchTest.java
+++ Infrastructure/src/main/java/com/ascklrt/infrastructure/middle/elasticsearch/demo/ElasticSearchTest.java
@@ -1,10 +1,13 @@
 package com.ascklrt.infrastructure.middle.elasticsearch.demo;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,13 +33,35 @@ public class ElasticSearchTest {
     }
 
     @Test
-    void createIndex() throws IOException {
+    void createHotelIndex() throws IOException {
         // 2.1 创建索引库
         CreateIndexRequest create = new CreateIndexRequest("hotel");
         // 2.2 准备请求的参数，DSL语句
         create.source(DSLConstans.MAPPING_TEMPLATE, XContentType.JSON);
         // 2.3 发送请求
         esClient.indices().create(create, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void deleteHotelIndex() throws IOException {
+        // 2.1 创建索引库
+        DeleteIndexRequest delete = new DeleteIndexRequest("hotel");
+        // 2.3 发送请求
+        esClient.indices().delete(delete, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void existHotelIndex() throws IOException {
+        GetIndexRequest getIndexRequest = new GetIndexRequest("hotel");
+        boolean exists = esClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+        System.out.println("index hotel is " + (exists ? "exist" : "not exist"));
+    }
+
+    @Test
+    void putDockment() throws IOException
+        IndexRequest put = new IndexRequest("hotel").id("1");
+        put.source("{\"name\": \"Jack\", \"age\" : \"21\"}", XContentType.JSON);
+        esClient.index(put, RequestOptions.DEFAULT);
     }
 
     void close() throws IOException {
